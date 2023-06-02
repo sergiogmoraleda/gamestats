1bca437ffaf13431c431c47dcb12d503715270ab
sergiogmoraleda <sergio.g.moraleda@gmail.com>
2023-06-02 21:45:52 +0200

    upd

--- users/schema.py
+++ users/schema.py
@@ -25,10 +25,36 @@ class Query(graphene.ObjectType):
         search=graphene.String(),
         first = graphene.Int(),
         skip= graphene.Int(),
+        limit=graphene.Int(),
         )
+    
+    top_users = graphene.List(
+        UserType,
+        property=graphene.String(),
+        limit=graphene.Int()
+    )
+
+    user_by_username = graphene.Field(UserType, username=graphene.String(required=True))
+
+     
+    def resolve_top_users(self, info, property=None, limit=None):
+        
+        if property == "headshotAccuracy":
+            order_by = "-stats__headshotAccuracy"
+        elif property == "wins":
+            order_by = "-stats__wins"
+        else:
+            order_by = None
+        
+        if order_by:
+            qs = User.objects.all().order_by(order_by)[:limit]
+        else:
+            qs = User.objects.all()[:limit]
+
+        return qs
 
 
-    def resolve_users(self, info, search=None,first= None, skip= None, **kwargs):
+    def resolve_users(self, info, search=None,first= None,limit=None, skip= None, **kwargs):
 
         qs = User.objects.all()
 
@@ -46,8 +72,17 @@ class Query(graphene.ObjectType):
         if first:
             qs = qs[:first]
 
-        return qs
+        if limit:  # Ordenar por headshotAccuracy de mayor a menor
+            qs = qs.order_by('-stats__headshotAccuracy')[:limit]
 
+        return qs
+    
+    def resolve_user_by_username(self, info, username):
+        try:
+            user = User.objects.get(username=username)
+            return user
+        except User.DoesNotExist:
+            raise GraphQLError("No existe un usuario con ese nombre de usuario.")
         
 
 # MUTATIONS
